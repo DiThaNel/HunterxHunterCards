978d30f627b90143f1e584741cfced0b2c3bddb7
DiThaNel <gabrielgoncalves4500@gmail.com>
2019-08-17 02:43:02 +0100

    Update

--- src/Components/Personajes.js
+++ src/Components/Personajes.js
@@ -53,7 +53,7 @@ const useStyles = makeStyles(theme => ({
     CardContent: {
       borderTop:'double 3px #979797',
       textAlign:'justify',
-      backgroundImage:"url('https://github.com/DiThaNel/HunterxHunterCards/src/public/img/CardBg.png')",
+      backgroundImage:"url('https://github.com/DiThaNel/HunterxHunterCards/blob/master/public/img/CardBg.jpg')",
       backgroundSize:'contain',
     },
     Typography: {
