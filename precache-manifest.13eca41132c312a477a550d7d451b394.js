bb1b41a234bc78bdccecdbb3e47166dac5198633
DithaNel <gabrielgoncalves4500@gmail.com>
2022-01-05 07:16:37 +0000

    Updates

---
+++ precache-manifest.13eca41132c312a477a550d7d451b394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f72131a4826b7f362d43bcb8f2fedf1",
+    "url": "/HunterxHunterCards/index.html"
+  },
+  {
+    "revision": "8110aaaca7285e0ac13f",
+    "url": "/HunterxHunterCards/static/css/2.246d7c7d.chunk.css"
+  },
+  {
+    "revision": "a617a7087dd78b3e7aea",
+    "url": "/HunterxHunterCards/static/css/main.18baf2e3.chunk.css"
+  },
+  {
+    "revision": "8110aaaca7285e0ac13f",
+    "url": "/HunterxHunterCards/static/js/2.a8d74501.chunk.js"
+  },
+  {
+    "revision": "a617a7087dd78b3e7aea",
+    "url": "/HunterxHunterCards/static/js/main.a959a0a5.chunk.js"
+  },
+  {
+    "revision": "ebd4db816ae7c711724c",
+    "url": "/HunterxHunterCards/static/js/runtime~main.060117ed.js"
+  }
+]);
